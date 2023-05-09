7d933656bdc2a75424451047dc5075ac195a3091
Florian Perrier <florian.fp.perrier@gmail.com>
2023-05-09 12:02:32 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './prisma-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
+
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  const config = new DocumentBuilder()
+    .setTitle('Ticket API')
+    .setDescription('API for a concert ticket app')
+    .setVersion('1.0')
+    .addTag('user')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
